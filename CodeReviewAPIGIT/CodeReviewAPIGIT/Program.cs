84dd09c4fe8b48e2fbc236a50ca55e771afcb6ca
Gael06 <gael.sauzet@binarytag.com>
2015-07-20 15:34:31 +0100

    Dub-46: commit info test

--- CodeReviewAPIGIT/CodeReviewAPIGIT/Program.cs
+++ CodeReviewAPIGIT/CodeReviewAPIGIT/Program.cs
@@ -7,6 +7,8 @@ namespace CodeReviewAPIGIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            var firstVar = string.Format("{0} commit infos", Console.ReadLine());
+            Console.WriteLine(firstVar);
             Console.ReadLine();
         }
     }
